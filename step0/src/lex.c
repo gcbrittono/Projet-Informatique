814429c4aee11c79c93b1413e72d391951da577e
gcbrittono <gcbrittono@unal.edu.co>
2018-10-06 13:21:40 +0200

    Actualisation du fichier lex.c et adition du fichier instructions_test.s sur tests

--- step0/src/lex.c
+++ step0/src/lex.c
@@ -35,18 +35,66 @@ char* getNextToken(char** token, char* current_line) {
     int token_size=0;
 
     /* check input parameter*/ 
-    if (current_line ==NULL) return NULL;
+    	if (current_line ==NULL) return NULL;
 
     /* eats any blanks before the token*/
-    while (*start!='\0' && isblank(*start)){
-	start++;
+	while ((*start!='\0' && isblank(*start) )){
+		start++;
     }	
-    /* go till next blank or end*/ 
+
+    /* go till end of a lexeme */ 
     end=start; 
 
-    while (*end!='\0' && !isblank(*end)){
-	end++; 
-    }
+	switch(*start){
+		case ('#'):
+			while (*end!='\n')
+				end++;
+			while (*end!='\0' && !isblank(*end))
+				end++;
+			break; /*eats any blanks or end of words or enter at the end of a comment*/
+	
+		case (':') :
+			end++;
+			break;
+		case (',') :
+			end++;
+			break;
+		case ('(') :
+			end++;
+			break;
+		case (')') :
+			end++;
+			break;
+		case ('\n') :
+			end++;
+			break;
+		case ('"'):
+			end++;
+			while (*end!='"'){
+				if ((*end=='\\') && (*(end++)=='"')){
+					end++;
+				}
+				end++;
+			}
+			end++;
+			break;
+
+		default:
+			while (*end!='\0' && !isblank(*end)){	
+				if (*end==':' || *end==',' || *end=='(' || *end==')' || *end=='"' || *end=='#'){
+					token_size=end-start;
+    					if (token_size>0){
+						*token 	= calloc(token_size+1,sizeof(*start));
+						strncpy(*token,start,token_size);
+						(*token)[token_size]='\0';       
+						return end;
+					}
+				}
+				end++;
+			}
+	}
+
+
     /*compute size : if zero there is no more token to extract*/ 	
     token_size=end-start;
     if (token_size>0){
@@ -68,6 +116,7 @@ char* getNextToken(char** token, char* current_line) {
  * @brief This function performs lexical analysis of one standardized line.
  *
  */
+
 void lex_read_line( char *line, int nline) {
     char* token = NULL;
     char* current_address=line;
@@ -75,13 +124,173 @@ void lex_read_line( char *line, int nline) {
 
     /* TODO : faire l'analyse lexical de chaque token ici et les ajouter dans une collection*/
     /* ATTENTION: getNextToken est à recoder completement*/
-    while( (current_address= getNextToken(&token, current_address)) != NULL){ 
+    while( (current_address= getNextToken(&token, current_address)) != NULL){
+    
+  	/* definition des etats*/
+	enum {INIT, COMMENTAIRE, SYMBOLE, DIRECTIVE, REGISTRE,  DEUX_POINTS, VIRGULE, SAUT_DE_LIGNE, HEXA_DEBUT, OCTATE, DECIMAL_ZEROS, DECIMAL, TERM, HEXA, CITATION};
+  /*mise en oeuvre de lautomate*/
+
+  int S = INIT; /*Definition detat de lautomate*/
+  char* c=token; /*caractere analyse courant*/
+ 
+while (*c!='\0'){
+  switch(S) 
+  {
+  	case INIT:
+	
+		/*if(isascii(c)){*/
+			if(isdigit(*c)){/*il separe les chiffres des caracteres*/
+				S = (*c=='0')?DECIMAL_ZEROS : DECIMAL;	
+			}
+			else if (isspace(*c)) S=INIT;
+			else if(isalpha(*c)){
+				S = SYMBOLE;
+			}
+			else if (iscntrl(*c)){
+				if (*c == '\n') S = SAUT_DE_LIGNE;
+				else  printf ("Erreur1 ");
+			}
+			else{
+				switch(*c){
+					case '#':
+						S = COMMENTAIRE;
+						break;
+					case '_':
+						S = SYMBOLE;
+						break;
+					case '.':
+						S = DIRECTIVE;
+						break;
+					case '$':
+						S = REGISTRE;
+						break;
+					case ':':
+						S = DEUX_POINTS;
+						break;
+					case ',':
+						S = VIRGULE;
+						break;
+					case '(':
+						S = SYMBOLE;
+						break;
+					case ')':
+						S = SYMBOLE;
+						break;
+					case '"':
+						S = CITATION;
+						break;
+					case '-':
+						S = DECIMAL;
+						break;
+					default:
+						 printf ("Erreur2 ");
+				}
+			}
+		/*}*/
+		/*else printf ("Erreur ");*/
+		break;
+		
+	case COMMENTAIRE:
+		break;
+		
+	case SYMBOLE:
+		if (isalpha(*c)) S = SYMBOLE;
+		else if (*c == ':'||*c == '.') S = TERM;
+		else if (isspace(*c)) S = TERM;
+		else printf ("Erreur 3");
+		break;
+		
+	case DIRECTIVE:
+		if (isalpha(*c)) S = DIRECTIVE;
+		else if(isspace(*c)) S = TERM;
+		else printf ("Erreur 4");
+		break;
+		
+	case REGISTRE:
+		if (isalnum(*c)) S = REGISTRE;
+		else if (*c == ',') S = TERM;
+		else if (isspace(*c)) S = TERM;
+		else printf ("Erreur 5");
+		break;
+		
+	case DEUX_POINTS:
+		if (isspace(*c)) S = TERM;
+		else printf ("Erreur 6");
+		break;
+		
+	case VIRGULE:
+		if (isspace(*c)) S = TERM;
+		else printf ("Erreur7");
+		break;
+		
+	case SAUT_DE_LIGNE:
+		S = TERM;
+		break;
+		
+	case DECIMAL_ZEROS:
+		if(*c == 'x') S = HEXA_DEBUT;
+		else if (*c == '\n') S = TERM;
+		else if (isspace(*c)) S = TERM;
+		if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
+		else printf("Erreur 8");
+		break;
+
+	case HEXA_DEBUT:
+		if(isxdigit(*c)) S = HEXA;
+		else printf("Erreur9");
+		break;
+
+	case OCTATE:
+		if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
+		else if(isspace(*c) && *c == '\n') S = TERM;
+		else printf("Erreur10");
+		break;
+
+	case DECIMAL:
+		if(isdigit(*c)) S = DECIMAL;
+		else if(isspace(*c) && *c == '\n') S = TERM;
+		else printf("Erreur11");
+		break;
+
+	case HEXA:
+		if(isxdigit(*c)) S = HEXA;
+		else if(isspace(*c) && *c == '\n') S = TERM;
+		else printf("Erreur12");
+		break;
+	case TERM:
+		break;	
+
+	case CITATION:
+		break;
+		
+	
+  
+  
+  }
+c++;
+ } 
+  
+  
+ printf("%d : ",S); 
+        puts(token);
+    }
+	
+    return;
+}
+/*void lex_read_line( char *line, int nline) {
+    char* token = NULL;
+    char* current_address=line;*/
+
+
+    /* TODO : faire l'analyse lexical de chaque token ici et les ajouter dans une collection*/
+    /* ATTENTION: getNextToken est à recoder completement*/
+/*    while( (current_address= getNextToken(&token, current_address)) != NULL){ 
 
         puts(token);
     }
 
     return;
-}
+}*/
 
 /**
  * @param file Assembly source code file name.
@@ -111,6 +320,7 @@ void lex_load_file( char *file, unsigned int *nlines ) {
         if ( NULL != fgets( line, STRLEN-1, fp ) ) {
             line[strlen(line)-1] = '\0';  /* eat final '\n' */
             (*nlines)++;
+		/*printf("%d \n",*nlines);*/
 
             if ( 0 != strlen(line) ) {
                 lex_read_line(line,*nlines);
