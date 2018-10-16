c179a529e3653cf20a20d5b72e3a69c8d9d16f80
gcbrittono <gcbrittono@unal.edu.co>
2018-10-16 14:52:08 +0200

    Actualisation du fichier lex.c

--- step0/src/lex.c
+++ step0/src/lex.c
@@ -29,9 +29,9 @@ int estVide(File L){
 }
 
 /*fonction qui ajoute un maillon à la fin de la file contenant le lexeme lex, de type cat et qui est sur la ligne lig, à la liste L*/
-lex_t creerElement(char* lex, int cat, int lig){
+lex_t creerElement(char* lex, etat cat, int lig){
 	lex_t p;
-	strcpy(p.lexeme,lex);
+	p.lexeme=strdup(lex);
 	p.categorie=cat;
 	p.ligne=lig;
 	p.suiv=NULL;
@@ -59,11 +59,61 @@ File enfiler(lex_t L, File F){
 /*afiche le premier élément de la file*/
 void afficherFile(File F){
 	if (estVide(F)){ /*cas ou la file est vide, on sort de la fonction*/
-		printf("la file est vide");
+		printf("la file est vide \n");
 		return;
 	}
-	printf("Ligne %d : [ %d ] : %s \n",F->suiv->ligne, F->suiv->categorie,F->suiv->
-lexeme);
+	char* etats;
+	switch(F->suiv->categorie){
+		case 0:
+			etats="INIT";
+			break;
+		case 1:
+			etats="COMMENTAIRE";
+			break;
+		case 2:
+			etats="SYMBOLE";
+			break;
+		case 3:
+			etats="DIRECTIVE";
+			break;
+		case 4:
+			etats="REGISTRE";
+			break;
+		case 5:
+			etats="DEUX_POINTS";
+			break;
+		case 6:
+			etats="VIRGULE";
+			break;
+		case 7:
+			etats="SAUT_DE_LIGNE";
+			break;
+		case 8:
+			etats="HEXA_DEBUT";
+			break;
+		case 9:
+			etats="OCTATE";
+			break;
+		case 10:
+			etats="DECIMAL_ZEROS";
+			break;
+		case 11:
+			etats="DECIMAL";
+			break;
+		case 12:
+			etats="TERM";
+			break;
+		case 13:
+			etats="HEXA";
+			break;
+		case 14:
+			etats="CITATION";
+			break;
+		case 15:
+			etats="PARENTHESE";
+			break;
+	}
+	printf("Ligne %d : [ %s ] : %s \n",F->suiv->ligne, etats, F->suiv->lexeme);
 }
 
 
@@ -77,20 +127,21 @@ lexeme);
 
 /* note that MIPS assembly supports distinctions between lower and upper case*/
 char* getNextToken(char** token, char* current_line) {
-    char* start = current_line; 
-    char* end=NULL; 
-    int token_size=0;
+    	char* start = current_line; 
+    	char* end=NULL; 
+    	int token_size=0;
 
     /* check input parameter*/ 
-    	if (current_line ==NULL) return NULL;
+    	if (current_line ==NULL) 
+		return NULL;
 
     /* eats any blanks before the token*/
 	while ((*start!='\0' && isblank(*start) )){
 		start++;
-    }	
+    	}	
 
     /* go till end of a lexeme */ 
-    end=start; 
+    	end=start; 
 
 	switch(*start){
 		case ('#'):
@@ -143,14 +194,14 @@ char* getNextToken(char** token, char* current_line) {
 
 
     /*compute size : if zero there is no more token to extract*/ 	
-    token_size=end-start;
-    if (token_size>0){
-	*token 	= calloc(token_size+1,sizeof(*start));
-	strncpy(*token,start,token_size);
-	(*token)[token_size]='\0';       
-	return end;
-    } 
-    return NULL;
+    	token_size=end-start;
+    	if (token_size>0){
+		*token 	= calloc(token_size+1,sizeof(*start));
+		strncpy(*token,start,token_size);
+		(*token)[token_size]='\0';       
+		return end;
+    	} 
+    	return NULL;
 }
 
 
@@ -165,209 +216,167 @@ char* getNextToken(char** token, char* current_line) {
  */
 
 void lex_read_line( char *line, int nline, File F) {
-    char* token = NULL;
-    char* current_address=line;
+    	char* token = NULL;
+    	char* current_address=line;
 
 
     /* TODO : faire l'analyse lexical de chaque token ici et les ajouter dans une collection*/
     /* ATTENTION: getNextToken est à recoder completement*/
-    while( (current_address= getNextToken(&token, current_address)) != NULL){
+    	while( (current_address= getNextToken(&token, current_address)) != NULL){
     
   	/* definition des etats*/
-	enum {INIT, COMMENTAIRE, SYMBOLE, DIRECTIVE, REGISTRE,  DEUX_POINTS, VIRGULE, SAUT_DE_LIGNE, HEXA_DEBUT, OCTATE, DECIMAL_ZEROS, DECIMAL, TERM, HEXA, CITATION};
+	/*enum {INIT, COMMENTAIRE, SYMBOLE, DIRECTIVE, REGISTRE,  DEUX_POINTS, VIRGULE, SAUT_DE_LIGNE, HEXA_DEBUT, OCTATE, DECIMAL_ZEROS, DECIMAL, TERM, HEXA, CITATION};*/
   /*mise en oeuvre de lautomate*/
 
-  int S = INIT; /*Definition detat de lautomate*/
-  char* c=token; /*caractere analyse courant*/
+  	etat S = INIT; /*Definition detat de lautomate*/
+  	char* c=token; /*caractere analyse courant*/
  
-while (*c!='\0'){
-  switch(S) 
-  {
-  	case INIT:
+	while (*c!='\0'){
+  		switch(S) {
+  			case INIT:
 	
 		/*if(isascii(c)){*/
-			if(isdigit(*c)){/*il separe les chiffres des caracteres*/
-				S = (*c=='0')?DECIMAL_ZEROS : DECIMAL;	
-			}
-			else if (isspace(*c)) S=INIT;
-			else if(isalpha(*c)){
-				S = SYMBOLE;
-			}
-			else if (iscntrl(*c)){
-				if (*c == '\n') S = SAUT_DE_LIGNE;
-				else  printf ("Erreur INIT ");
-			}
-			else{
-				switch(*c){
-					case '#':
-						S = COMMENTAIRE;
-						break;
-					case '_':
-						S = SYMBOLE;
-						break;
-					case '.':
-						S = DIRECTIVE;
-						break;
-					case '$':
-						S = REGISTRE;
-						break;
-					case ':':
-						S = DEUX_POINTS;
-						break;
-					case ',':
-						S = VIRGULE;
-						break;
-					case '(':
-						S = SYMBOLE;
-						break;
-					case ')':
-						S = SYMBOLE;
-						break;
-					case '"':
-						S = CITATION;
-						break;
-					case '-':
-						S = DECIMAL;
-						break;
-					default:
-						 printf ("Erreur INIT ");
+				if(isdigit(*c)){/*il separe les chiffres des caracteres*/
+					S = (*c=='0')?DECIMAL_ZEROS : DECIMAL;	
 				}
-			}
-		/*}*/
-		/*else printf ("Erreur ");*/
-		break;
+				else if (isspace(*c)) S=INIT;
+				else if(isalpha(*c)){
+					S = SYMBOLE;
+				}
+				else if (iscntrl(*c)){
+					if (*c == '\n') S = SAUT_DE_LIGNE;
+					else  printf ("Erreur INIT ");
+				}
+				else{
+					switch(*c){
+						case '#':
+							S = COMMENTAIRE;
+							break;
+						case '_':
+							S = SYMBOLE;
+							break;
+						case '.':
+							S = DIRECTIVE;
+							break;
+						case '$':
+							S = REGISTRE;
+							break;
+						case ':':
+							S = DEUX_POINTS;
+							break;
+						case ',':
+							S = VIRGULE;
+							break;
+						case '(':
+							S = PARENTHESE;
+							break;
+						case ')':
+							S = PARENTHESE;
+							break;
+						case '"':
+							S = CITATION;
+							break;
+						case '-':
+							S = DECIMAL;
+							break;
+						default:
+						 	printf ("Erreur INIT ");
+					}
+				}
+				/*}*/
+				/*else printf ("Erreur ");*/
+				break;
 		
-	case COMMENTAIRE:
-		break;
+			case COMMENTAIRE:
+				break;
 		
-	case SYMBOLE:
-		if (isalpha(*c)) S = SYMBOLE;
-		else if (*c == ':'||*c == '.') S = TERM;
-		else if (isspace(*c)) S = TERM;
-		else printf ("Erreur SYMBOLE");
-		break;
+			case SYMBOLE:
+				if (isalpha(*c)) S = SYMBOLE;
+				else if (*c == ':'||*c == '.') S = TERM;
+				else if (isspace(*c)) S = TERM;
+				else printf ("Erreur SYMBOLE");
+				break;
 		
-	case DIRECTIVE:
-		if (isalpha(*c)) S = DIRECTIVE;
-		else if(isspace(*c)) S = TERM;
-		else printf ("Erreur DIRECTIVE");
-		break;
+			case DIRECTIVE:
+				if (isalpha(*c)) S = DIRECTIVE;
+				else if(isspace(*c)) S = TERM;
+				else printf ("Erreur DIRECTIVE");
+				break;
 		
-	case REGISTRE:
-		if (isalnum(*c)) S = REGISTRE;
-		else if (*c == ',') S = TERM;
-		else if (isspace(*c)) S = TERM;
-		else printf ("Erreur REGISTRE");
-		break;
+			case REGISTRE:
+				if (isalnum(*c)) S = REGISTRE;
+				else if (*c == ',') S = TERM;
+				else if (isspace(*c)) S = TERM;
+				else printf ("Erreur REGISTRE");
+				break;
 		
-	case DEUX_POINTS:
-		if (isspace(*c)) S = TERM;
-		else printf ("Erreur DEUX-POINTS");
-		break;
+			case DEUX_POINTS:
+				if (isspace(*c)) S = TERM;
+				else printf ("Erreur DEUX-POINTS");
+				break;
 		
-	case VIRGULE:
-		if (isspace(*c)) S = TERM;
-		else printf ("Erreur VIRGULE");
-		break;
-		
-	case SAUT_DE_LIGNE:
-		S = TERM;
-		break;
-		
-	case DECIMAL_ZEROS:
-		if(*c == 'x') S = HEXA_DEBUT;
-		else if (*c == '\n') S = TERM;
-		else if (isspace(*c)) S = TERM;
-		if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
-		else printf("Erreur DECIMAL-ZEROS");
-		break;
+			case PARENTHESE:
+				if (isspace(*c)) S = TERM;
+				else printf ("Erreur PARENTHESE");
+				break;
 
-	case HEXA_DEBUT:
-		if(isxdigit(*c)) S = HEXA;
-		else printf("Erreur HEXA_DEBUT");
-		break;
+			case VIRGULE:
+				if (isspace(*c)) S = TERM;
+				else printf ("Erreur VIRGULE");
+				break;
+		
+			case SAUT_DE_LIGNE:
+				S = TERM;
+				break;
+		
+			case DECIMAL_ZEROS:
+				if(*c == 'x') S = HEXA_DEBUT;
+				else if (*c == '\n') S = TERM;
+				else if (isspace(*c)) S = TERM;
+				if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
+				else printf("Erreur DECIMAL-ZEROS");
+				break;
 
-	case OCTATE:
-		if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
-		else if(isspace(*c) && *c == '\n') S = TERM;
-		else printf("Erreur OCTATE");
-		break;
+			case HEXA_DEBUT:
+				if(isxdigit(*c)) S = HEXA;
+				else printf("Erreur HEXA_DEBUT");
+				break;
 
-	case DECIMAL:
-		if(isdigit(*c)) S = DECIMAL;
-		else if(isspace(*c) && *c == '\n') S = TERM;
-		else printf("Erreur DECIMAL");
-		break;
+			case OCTATE:
+				if (isdigit(*c)||((0<=*c)<=7))S = OCTATE;
+				else if(isspace(*c) && *c == '\n') S = TERM;
+				else printf("Erreur OCTATE");
+				break;
 
-	case HEXA:
-		if(isxdigit(*c)) S = HEXA;
-		else if(isspace(*c) && *c == '\n') S = TERM;
-		else printf("Erreur HEXA");
-		break;
-	case TERM:
-		break;	
+			case DECIMAL:
+				if(isdigit(*c)) S = DECIMAL;
+				else if(isspace(*c) && *c == '\n') S = TERM;
+				else printf("Erreur DECIMAL");
+				break;
 
-	case CITATION:
-		break;
+			case HEXA:
+				if(isxdigit(*c)) S = HEXA;
+				else if(isspace(*c) && *c == '\n') S = TERM;
+				else printf("Erreur HEXA");
+				break;
+			case TERM:
+				break;	
+
+			case CITATION:
+				break;
 		
 	
   
   
-  }
-c++;
- } 
-/*lex_t t=creerElement("gjfjk", 4 ,5);*/
-/*F=enfiler(t,F);*/
- char* etats;
-switch(S){
-	case 1:
-		etats="INIT";
-		break;
-	case 2:
-		etats="COMMENTAIRE";
-		break;
-	case 3:
-		etats="SYMBOLE";
-		break;
-	case 4:
-		etats="DIRECTIVE";
-		break;
-	case 5:
-		etats="REGISTRE";
-		break;
-	case 6:
-		etats="DEUX_POINTS";
-		break;
-	case 7:
-		etats="VIRGULE";
-		break;
-	case 8:
-		etats="SAUT_DE_LIGNE";
-		break;
-	case 9:
-		etats="HEXA_DEBUT";
-		break;
-	case 10:
-		etats="OCTATE";
-		break;
-	case 11:
-		etats="DECIMAL_ZEROS";
-		break;
-	case 12:
-		etats="DECIMAL";
-		break;
-	case 13:
-		etats="TERM";
-		break;
-	case 14:
-		etats="HEXA";
-		break;
-	case 15:
-		etats="CITATION";
-		break;
-}
- printf("ligne %d : [%s] :",nline, etats); 
-        puts(token);
+  		}
+		c++;
+ 	} 
+
+	F=enfiler(creerElement(token, S ,nline),F);
+afficherFile(F);
+ 
+        /*puts(token);*/
+
     }
 	
     return;
@@ -394,37 +403,45 @@ switch(S){
  * @brief This function loads an assembly code from a file into memory.
  *
  */
-void lex_load_file( char *file, unsigned int *nlines ) {
+void lex_load_file( char *file, unsigned int *nlines, File F ) {
+	FILE        *fp   = NULL;
+	char         line[STRLEN]; /* original source line */
+	
 
-    FILE        *fp   = NULL;
-    char         line[STRLEN]; /* original source line */
-	File F=creerFile();
 
-
-    fp = fopen( file, "r" );
-    if ( NULL == fp ) {
+	fp = fopen( file, "r" );
+	if ( NULL == fp ) {
         /*macro ERROR_MSG : message d'erreur puis fin de programme ! */
-        ERROR_MSG("Error while trying to open %s file --- Aborts",file);
-    }
+        	ERROR_MSG("Error while trying to open %s file --- Aborts",file);
+    	}
 
-    *nlines = 0;
+    	*nlines = 0;
 
-    while(!feof(fp)) {
+    	while(!feof(fp)) {
 
         /*read source code line-by-line */
-        if ( NULL != fgets( line, STRLEN-1, fp ) ) {
-            line[strlen(line)-1] = '\0';  /* eat final '\n' */
-            (*nlines)++;
-		/*printf("%d \n",*nlines);*/
+        	if ( NULL != fgets( line, STRLEN-1, fp ) ) {
+            		line[strlen(line)-1] = '\0';  /* eat final '\n' */
+            		(*nlines)++;
+			/*printf("%d \n",*nlines);*/
 
-            if ( 0 != strlen(line) ) {
-                lex_read_line(line,*nlines,F);
-            }
-        }
-    }
+            		if ( 0 != strlen(line) ) {
+                		lex_read_line(line,*nlines,F);
+            		}
+        	}
+	
+    	}
 
-    fclose(fp);
-    return;
+/*affichage de la file de lexeme*/
+	/*File P=F;
+	do{
+		afficherFile(P);
+		P=P->suiv;
+	}while (P!=F);*/
+	
+/*fermeture du fichier et fin de fonction*/		
+    	fclose(fp);
+    	return;
 }
 
 
