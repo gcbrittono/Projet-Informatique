45855bc0bc121e721d84a8546d226ed4496b791d
gcbrittono <gcbrittono@unal.edu.co>
2018-11-20 16:29:32 +0100

    Adition des fonctions pour trouver les pseudoinstructions

--- step0/include/rel.h
+++ step0/include/rel.h
@@ -3,6 +3,10 @@
 
 
 #include <stdio.h>
+#include "lex.h"
+#include "gram.h"
+
+
 
 typedef enum {R_MIPS_32=2, R_MIPS_26=4, R_MIPS_HI16=5,
 R_MIPS_LO16=6} mode_rel;
@@ -11,15 +15,19 @@ typedef struct table_relocation {
 	char* nom_section;
 	unsigned int addr_relative;
     mode_rel mode_relocation;
-    symbol* pointeur; 
-};
+    struct table_relocation* pointeur; 
+}table_relocation;
 
 
 
 
+/*Fonction pour trouver un symbole et l'ajouté au tableu de relocation*/
+table_relocation  symbole_find(Symbole* symb);
 
+/*Fonction pour trouver un instruction et l'ajouté au tableu de relocation*/
+table_relocation  instruction_find(Instruction* inst);
 
-
+ListeG* trouver_pseudoinstruction(/*Instruction* inst*/ File listeInstr,Dico tableau[],int dec_text, int position);
 
 
 
--- step0/src/rel.c
+++ step0/src/rel.c
@@ -7,25 +7,7 @@
 
 
 
-Symbole* creerSymbole(char* lex,	etat cat, int lig, Section section ,int dec){
-	Symbole* p=malloc(sizeof(*p));
-	p->lexeme=strdup(lex);
-	p->type=cat;
-	p->ligne=lig;
-	p->decalage=dec;
-	p->sect=section;
-	return p;
-}
 
-typedef struct {
-	char* nom;
-	etat type;
-    char typ_inst;
-	int nbop;
-	int ligne;
-	unsigned int decalage;
-	Operande op[3];
-} Instruction;
 
 
 
@@ -33,35 +15,35 @@ typedef struct {
 R_MIPS_LO16=6} mode_rel;*/
 
 /*Fonction pour trouver un symbole*/
-void symbole_find(Symbole* symb){
+table_relocation  symbole_find(Symbole* symb){
     
     table_relocation tab_rel;
 
-        if(strcmp(symb->sect, ".data")==0){
+        if(symb->sect==0){
             tab_rel.nom_section = "data";
             tab_rel.addr_relative = symb->decalage;
             tab_rel.mode_relocation = 2;
-            tab_rel.pointeur = symb;
+         /*   tab_rel.pointeur = symb;*/
 				
 		}
-        else if(strcmp(symb->sect,".text")==0){
+        else if(symb->sect==2){
             tab_rel.nom_section = "text";
             tab_rel.addr_relative = symb->decalage;
             tab_rel.mode_relocation = 2;  /*Je ne suis pas sur si ca marcherá dans le cas text*/
-			tab_rel.pointeur = symb;	
+		/*	tab_rel.pointeur = symb;*/	
 		}
 
     
 }
 
 /*Fonction pour trouver un instruction*/
-void instruction_find(Instruction* inst){
+table_relocation  instruction_find(Instruction* inst){
     
     table_relocation tab_rel;
 
     tab_rel.nom_section = "text";
     tab_rel.addr_relative = inst->decalage;
-    tab_rel.pointeur = inst
+    /*tab_rel.pointeur = inst*/
 ;
     switch(inst->typ_inst) {
 
@@ -83,6 +65,26 @@ void instruction_find(Instruction* inst){
    }
 
 }
+
+
+/*Fonction pour parcourir une liste et trouver un pseudoinstruction*/
+ListeG* trouver_pseudoinstruction(/*Instruction* inst*/ File listeInstr,Dico tableau[],int dec_text, int position){
+	/*Liste pour aficher tous les pseudoinstructions trouvé*/	
+	ListeG* tableu_pseudoInstr;
+	Instruction *inst=malloc(sizeof(*inst));;
+	
+	while(listeInstr->suiv != NULL){
+		*inst = creerInstruction(listeInstr->lexeme, listeInstr->categorie,tableau[position].operands, listeInstr->ligne, dec_text);
+		if(inst->nom == "nop"||"lw"||"sw"||"move"||"neg"||"li"||"blt")
+			tableu_pseudoInstr = ajouterQueue(inst,tableu_pseudoInstr);
+	}
+
+	return tableu_pseudoInstr;
+}
+
+
+
+
 
 
 /*Fonction pour faire le decalage */
